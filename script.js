7a001d57c475cbd4d4f4eb65ee0c8e966f063d5b
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-09-28 15:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 //your code here
 
+// Function to sort band names in lexicographic order excluding articles
+function sortBandNames(bandNames) {
+  return bandNames.sort((a, b) => {
+    // Define a regular expression to match articles ('a', 'an', 'the') at the beginning of band names
+    const articleRegex = /^(a |an |the )/i;
+
+    // Remove articles from both band names for comparison
+    const nameA = a.replace(articleRegex, '');
+    const nameB = b.replace(articleRegex, '');
+
+    // Compare the names without articles in lexicographic order
+    return nameA.localeCompare(nameB);
+  });
+}
+
+// Example array of band names
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Aerosmith', 'The Beatles'];
+
+// Sort the band names
+const sortedBandNames = sortBandNames(bandNames);
+
+// Get the ul element by its id
+const ulElement = document.getElementById('band');
+
+// Create li elements and append them to the ul element
+sortedBandNames.forEach((bandName) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = bandName;
+  ulElement.appendChild(liElement);
+});
